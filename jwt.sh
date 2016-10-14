736fee680e60ceb90a647d3578c8dc63ffd7e358
Kedar Doshi <kedar@metamind.io>
2016-10-14 10:06:56 -0700

    outputted header, payload and private key for jwt.io troubleshooting

--- jwt.sh
+++ jwt.sh
@@ -7,15 +7,30 @@ fi
 keytool -v -importkeystore -srckeystore $1 -destkeystore privateKey.p12 -deststoretype PKCS12
 
 openssl pkcs12 -in privateKey.p12 -nocerts -nodes -out private_key
+echo
+echo private key:
+tail -n +5  private_key
+echo
 
-jwt1=`echo -n '{"alg":"RS256","typ":"JWT"}' | openssl base64 -e`
+header=`echo -n '{"alg":"RS256","typ":"JWT"}'`
+echo header:
+echo $header
+echo
 
-jwt2=`echo -n '{\
+jwt1=`echo -n $header | openssl base64 -e`
+
+
+payload=`echo -n '{\
 "iss":"'$2'",\
 "sub":"'$2'",\
 "aud":"'$4'/v1/oauth2/token",\
 "exp":'$(($(date +%s)+$3))',\
-"iat":'$(date +%s)'}' | openssl base64 -e`
+"iat":'$(date +%s)'}'`
+echo payload:
+echo $payload
+echo
+
+jwt2=`echo -n $payload | openssl base64 -e`
 
 jwt3=`echo -n "$jwt1.$jwt2" | tr -d '\n' | tr -d '=' | tr '/+' '_-'`
 
@@ -30,5 +45,6 @@ echo $jwt3.$jwt5
 echo
 echo "Your access token response:"
 echo
+
 curl -H "Content-type: application/x-www-form-urlencoded" -X POST "$4/v1/oauth2/token" -d \
 "grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=$jwt3.$jwt5" ; echo
